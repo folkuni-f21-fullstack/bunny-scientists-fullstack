f0ae34d1066d91b501977cf7c9c17543995a2b3c
Linus Eklind <76260367+Eklinden@users.noreply.github.com>
2022-10-12 15:43:53 +0200

    selected nav function added

--- src/components/MenuNav/MenuNav.tsx
+++ src/components/MenuNav/MenuNav.tsx
@@ -1,28 +1,80 @@
-import {useState} from 'react';
+import { useState } from "react";
 import "./MenuNav.scss";
 
 const MenuNav = () => {
-    // const [isActive, setIsActive] = useState("")
+  const [foodSelected, setfoodSelected] = useState("");
+  const [kidsSelected, setkidsSelected] = useState("");
+  const [beveragesSelected, setbeveragesSelected] = useState("");
+  const [dessertSelected, setdessertSelected] = useState("");
 
-    // function toggleActive() {
-    //     if(isActive == "active"){
-    //         setIsActive("")
-    //     } else {
-    //         setIsActive("active")
-    //     }
-    // }
-    return (
-        <nav className="menu-nav">
-            <ul>
-                <li><a href="#food">Mat</a></li>
-                <span className="vertical-line"></span>
-                <li><a href="#kids">Barn</a></li>
-                <span className="vertical-line"></span>
-                <li><a href="#beverages">Dryck</a></li>
-                <span className="vertical-line"></span>
-                <li><a href="#desserts">Efterrätt</a></li>
-            </ul>
-        </nav>
-    )
-}
-export default MenuNav
+  function toggleActive(e: any): void {
+    if (e.target.innerHTML === "Dryck") {
+      setbeveragesSelected("active");
+      setkidsSelected("");
+      setdessertSelected("");
+      setfoodSelected("");
+    } else if (e.target.innerHTML === "Barn") {
+      setbeveragesSelected("");
+      setkidsSelected("active");
+      setdessertSelected("");
+      setfoodSelected("");
+    } else if (e.target.innerHTML === "Mat") {
+      setbeveragesSelected("");
+      setkidsSelected("");
+      setdessertSelected("");
+      setfoodSelected("active");
+    } else if (e.target.innerHTML === "Efterrätt") {
+      setbeveragesSelected("");
+      setkidsSelected("");
+      setdessertSelected("active");
+      setfoodSelected("");
+    }
+    console.log(e);
+  }
+  return (
+    <nav className="menu-nav">
+      <ul>
+        <li>
+          <a
+            className={foodSelected}
+            onClick={(e) => toggleActive(e)}
+            href="#food"
+          >
+            Mat
+          </a>
+        </li>
+        <span className="vertical-line"></span>
+        <li>
+          <a
+            className={kidsSelected}
+            onClick={(e) => toggleActive(e)}
+            href="#kids"
+          >
+            Barn
+          </a>
+        </li>
+        <span className="vertical-line"></span>
+        <li>
+          <a
+            className={beveragesSelected}
+            onClick={(e) => toggleActive(e)}
+            href="#beverages"
+          >
+            Dryck
+          </a>
+        </li>
+        <span className="vertical-line"></span>
+        <li>
+          <a
+            className={dessertSelected}
+            onClick={(e) => toggleActive(e)}
+            href="#desserts"
+          >
+            Efterrätt
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default MenuNav;
