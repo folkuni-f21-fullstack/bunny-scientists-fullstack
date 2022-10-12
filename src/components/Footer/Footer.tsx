3fa2344fcfff76f629fb0052ec3d9f9ec11e5ef4
Linus Eklind <76260367+Eklinden@users.noreply.github.com>
2022-10-12 11:17:09 +0200

    Feature footer done

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,43 @@
+import {
+  AiFillFacebook,
+  AiFillInstagram,
+  AiFillLinkedin,
+} from "react-icons/ai";
+import { BiLogIn } from "react-icons/bi";
 import "./Footer.scss";
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <article>
+        <section>
+          <h2>Kontakt</h2>
+          <ul>
+            <li>Telefon: 070 999 99 99</li>
+            <li>Adress: Idagatan</li>
+          </ul>
+        </section>
+        <section>
+          <h2>Sociala medier</h2>
+          <ul className="socials">
+            <li>
+              <AiFillFacebook />
+            </li>
+            <li>
+              <AiFillInstagram />
+            </li>
+            <li>
+              <AiFillLinkedin />
+            </li>
+          </ul>
+        </section>
+        <a href="">
+          Admin inlog <BiLogIn />
+        </a>
+        <p>© 2022 All rights reserved Escargot Togo</p>
+      </article>
+    </footer>
+  );
 };
 
 export default Footer;
