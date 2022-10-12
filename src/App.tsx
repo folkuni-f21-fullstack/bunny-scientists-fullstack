d7626731fcf7e98f6406551dd207888aef9fe2df
Williamlof <83575469+Williamlof@users.noreply.github.com>
2022-10-12 10:00:36 +0200

    Grid-layout
    Added footer header components and homepage View.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import HomePage from "./views/homePage/HomePage";
 
 import "./App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
