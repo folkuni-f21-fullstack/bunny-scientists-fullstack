b973826379be77787929d4ee177b2044242a1d1e
Pellehakman <perhakman@outlook.com>
2022-10-12 13:36:16 +0200

    Update AboutPage.tsx

--- src/views/aboutPage/AboutPage.tsx
+++ src/views/aboutPage/AboutPage.tsx
@@ -2,6 +2,7 @@ import "../aboutPage/AboutPage.scss";
 import VD from "../../assets/staff/vd.jpg"
 import Reception from "../../assets/staff/reception.jpg"
 import Chef from "../../assets/staff/chef.jpg"
+import data from '../../data/data.json'
 
 const AboutPage = () => {
   return (
@@ -12,9 +13,9 @@ const AboutPage = () => {
       <p>Vi öppnade våra dörrar till restaurangen år 2018 där vi möttes av omedelbar och stor framgång. </p>
       <p>Efter att vi besöktes av självaste Konungen och drottning Silvia första gången bestämde vi oss för att bannlysa dem från verksamheten, sänka våra priser och få sniglar in i munnen på vanligt folk.</p>
       <h2 className="contact">Kontakt</h2>
-      <p>Idagatan 55, 656 95 Karlstad</p>
-      <p>070-112 12 12</p>
-      <p>info.escargottogo@snigelpost.com</p>
+      <p>{data.array.restaurant.address}</p>
+      <p>{data.array.restaurant.phone}</p>
+      <p>{data.array.restaurant.mail}</p>
       <div className="line"></div>
       <section className="staff">
         <div>
