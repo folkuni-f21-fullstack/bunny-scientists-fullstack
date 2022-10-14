6e5add6eb3db689191219e63e12725e7250845df
Pellehakman <perhakman@outlook.com>
2022-10-14 13:30:57 +0200

    Update data.ts

--- src/models/data.ts
+++ src/models/data.ts
@@ -1,7 +1,19 @@
+<<<<<<< Updated upstream
 export interface Menu {
 	id: string;
     name: string;
 	price: number;
     description: string;
     image: string;
+=======
+export interface MenuItem {
+    menuItem:{
+        id: string;
+        name: string;
+        price: number;
+        description: string;
+        image: string;
+    }
+	
+>>>>>>> Stashed changes
 };
