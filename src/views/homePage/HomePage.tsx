2640b264c6c56c1a8b43b4fbed730ba945fb1620
Linus Eklind <76260367+Eklinden@users.noreply.github.com>
2022-10-14 11:07:28 +0200

    genereal stylings done

--- src/views/homePage/HomePage.tsx
+++ src/views/homePage/HomePage.tsx
@@ -1,55 +1,47 @@
-import "./HomePage.scss";
-import MenuNav from "../../components/MenuNav/MenuNav";
-import Hero from "../../components/Hero/Hero"
-import MenuCard from "../../components/MenuCard/MenuCard";
-import Data from "../../data/data.json"
-import { MenuItem } from '../../models/data'
+import Hero from '../../components/Hero/Hero';
+import MenuCard from '../../components/MenuCard/MenuCard';
+import MenuNav from '../../components/MenuNav/MenuNav';
+import Data from '../../data/data.json';
+import { MenuItem } from '../../models/data';
+import './HomePage.scss';
 
 const HomePage = () => {
-  const itemsByCategory = Data.array.menu
-  const escargots = itemsByCategory[0]["menu-items"]
-  const kids = itemsByCategory[1]["menu-items"]
-  const drinks = itemsByCategory[2]["menu-items"]
-  const desserts = itemsByCategory[3]["menu-items"]
-  // console.log(escargots)
-  return (
-    <main className="home-page">
-      <Hero/>
-      <MenuNav/>
-      <h2 id="sniglar">Sniglar</h2>
-      <section className="cards-container">
-        {
-          escargots.map((menuItem, i:number)=>{
-            return (<MenuCard menuItem={menuItem} key={i}/>)
-          })
-        }
-      </section>
-      <h2 id="kids">Barn</h2>
-      <section className="cards-container">
-        {
-          kids.map((menuItem, i:number)=>{
-            return (<MenuCard menuItem={menuItem} key={i}/>)
-          })
-        }
-      </section>
-      <h2 id="beverages">Dryck</h2>
-      <section className="cards-container">
-        {
-          drinks.map((menuItem, i:number)=>{
-            return (<MenuCard menuItem={menuItem} key={i}/>)
-          })
-        }
-      </section>
-      <h2 id="desserts">Efterätter</h2>
-      <section className="cards-container">
-      {
-          desserts.map((menuItem, i:number)=>{
-            return (<MenuCard menuItem={menuItem} key={i}/>)
-          })
-        }
-      </section>
-    </main>
-  )
+	const itemsByCategory = Data.array.menu;
+	const escargots = itemsByCategory[0]['menu-items'];
+	const kids = itemsByCategory[1]['menu-items'];
+	const drinks = itemsByCategory[2]['menu-items'];
+	const desserts = itemsByCategory[3]['menu-items'];
+	// console.log(escargots)
+	return (
+		<main className='home-page'>
+			<Hero />
+			<MenuNav />
+			<h2>Sniglar</h2>
+			<section className='cards-container'>
+				{escargots.map((menuItem, i: number) => {
+					return <MenuCard menuItem={menuItem} key={i} />;
+				})}
+			</section>
+			<h2 id='kids'>Barn</h2>
+			<section className='cards-container'>
+				{kids.map((menuItem, i: number) => {
+					return <MenuCard menuItem={menuItem} key={i} />;
+				})}
+			</section>
+			<h2 id='beverages'>Dryck</h2>
+			<section className='cards-container'>
+				{drinks.map((menuItem, i: number) => {
+					return <MenuCard menuItem={menuItem} key={i} />;
+				})}
+			</section>
+			<h2 id='desserts'>Efterätter</h2>
+			<section className='cards-container'>
+				{desserts.map((menuItem, i: number) => {
+					return <MenuCard menuItem={menuItem} key={i} />;
+				})}
+			</section>
+		</main>
+	);
 };
 
 export default HomePage;
