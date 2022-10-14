931462a6a49f434ce01d98f3a4a8b807778b2a78
Linus Eklind <76260367+Eklinden@users.noreply.github.com>
2022-10-14 14:25:26 +0200

    make-footer-resposiv done

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,44 +1,44 @@
 import {
-  AiFillFacebook,
-  AiFillInstagram,
-  AiFillLinkedin,
-} from "react-icons/ai";
-import { BiLogIn } from "react-icons/bi";
-import "./Footer.scss";
-import data from '../../data/data.json'
+	AiFillFacebook,
+	AiFillInstagram,
+	AiFillLinkedin,
+} from 'react-icons/ai';
+import { BiLogIn } from 'react-icons/bi';
+import data from '../../data/data.json';
+import './Footer.scss';
 
 const Footer = () => {
-  return (
-    <footer>
-      <article>
-        <section>
-          <h2>Kontakt</h2>
-          <ul>
-            <li>Telefon: {data.array.restaurant.phone}</li>
-            <li>Adress: {data.array.restaurant.address}</li>
-          </ul>
-        </section>
-        <section>
-          <h2>Sociala medier</h2>
-          <ul className="socials">
-            <li>
-              <AiFillFacebook />
-            </li>
-            <li>
-              <AiFillInstagram />
-            </li>
-            <li>
-              <AiFillLinkedin />
-            </li>
-          </ul>
-        </section>
-        <a href="">
-          Admin inlog <BiLogIn />
-        </a>
-        <p>© 2022 All rights reserved Escargot Togo</p>
-      </article>
-    </footer>
-  );
+	return (
+		<footer>
+			<article>
+				<section>
+					<h2>Kontakt</h2>
+					<ul>
+						<li>Telefon: {data.array.restaurant.phone}</li>
+						<li>Adress: {data.array.restaurant.address}</li>
+					</ul>
+				</section>
+				<section>
+					<h2>Sociala medier</h2>
+					<ul className='socials'>
+						<li>
+							<AiFillFacebook />
+						</li>
+						<li>
+							<AiFillInstagram />
+						</li>
+						<li>
+							<AiFillLinkedin />
+						</li>
+					</ul>
+				</section>
+			</article>
+			<a href=''>
+				Admin inlog <BiLogIn />
+			</a>
+			<p>© 2022 All rights reserved Escargot Togo</p>
+		</footer>
+	);
 };
 
 export default Footer;
