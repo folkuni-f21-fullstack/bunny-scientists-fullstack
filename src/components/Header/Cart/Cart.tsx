c1ceb3edee33a7dd96507913f29c82afb008d317
Williamlof <83575469+Williamlof@users.noreply.github.com>
2022-10-20 09:45:36 +0200

    Cart v1.0
    Cart styling

--- src/components/Header/Cart/Cart.tsx
+++ src/components/Header/Cart/Cart.tsx
@@ -1,5 +1,5 @@
 import "./Cart.scss";
-
+import { IoMdTrash } from "react-icons/io";
 interface Props {
   cartMenuClass: string;
   cartOpenClass: string;
@@ -9,14 +9,65 @@ const Cart = ({ cartMenuClass, cartOpenClass }: Props) => {
   return (
     <div>
       <div className={cartMenuClass}>
-        <h2>PRODUKT</h2>
-        <ul className="products">
-          <li className="product-in-cart"></li>
-          <li className="product-in-cart"></li>
-          <li className="product-in-cart"></li>
-          <li className="product-in-cart"></li>
-          <li className="product-in-cart"></li>
-        </ul>
+        <div>
+          <h2>PRODUKT</h2>
+          <ul className="products">
+            <li className="product-in-cart">
+              <h3>Tradition</h3>
+              <p>249 kr</p>
+              <div className="add-remove-container">
+                <button className="decrease">-</button>
+                <p>2</p>
+                <button className="increase">+</button>
+              </div>
+            </li>
+            <li className="product-in-cart">
+              <h3>Tradition</h3>
+              <p>249 kr</p>
+              <div className="add-remove-container">
+                <button className="decrease">-</button>
+                <p>2</p>
+                <button className="increase">+</button>
+              </div>
+            </li>
+            <li className="product-in-cart">
+              <h3>Tradition</h3>
+              <p>249 kr</p>
+              <div className="add-remove-container">
+                <button className="decrease">-</button>
+                <p>2</p>
+                <button className="increase">+</button>
+              </div>
+            </li>
+            <li className="product-in-cart">
+              <h3>Tradition</h3>
+              <p>249 kr</p>
+              <div className="add-remove-container">
+                <button className="decrease">-</button>
+                <p>2</p>
+                <button className="increase">+</button>
+              </div>
+            </li>
+            <li className="product-in-cart">
+              <h3>Tradition</h3>
+              <p>249 kr</p>
+              <div className="add-remove-container">
+                <button className="decrease">-</button>
+                <p>2</p>
+                <button className="increase">+</button>
+              </div>
+            </li>
+          </ul>
+          <div className="empty-cart-container">
+            <h1>TÖM VARUKORG</h1>
+            <IoMdTrash className="trash-icon" />
+          </div>
+          <div className="total-container">
+            <h2>TOTALT (INKL. MOMS)</h2>
+            <h1>523 kr</h1>
+          </div>
+          <button className="checkout">TILL KASSAN</button>
+        </div>
       </div>
     </div>
   );
