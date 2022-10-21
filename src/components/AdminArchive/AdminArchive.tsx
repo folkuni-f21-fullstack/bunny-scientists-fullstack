d0a2e702cd34e862cf627010cb1cd754259e622e
Ida Larsson <77833853+IdaLarsson90@users.noreply.github.com>
2022-10-21 15:23:53 +0200

    admin update

--- src/components/AdminArchive/AdminArchive.tsx
+++ src/components/AdminArchive/AdminArchive.tsx
@@ -1,32 +1,42 @@
 import "./AdminArchive.scss";
-import { useState } from "react";
-
-import Data from '../../data/data.json';
 import OrderItem from "../OrderItem/OrderItem";
 
 const archivedOrders = [
-  {id: 9, time: "20:39:16", ordernr: 4524, order: [{type: "Vitlökssnigel", quantity: 2}, {type: "Currysnigel", quantity: 4}], customer: [{name: "Linus Pellesson"},{phone: "074-21 23 124"}]}, 
-  {id: 10, time: "20:39:16", ordernr: 4523, order: [{type: "Vitlökssnigel", quantity: 2}, {type: "Grässnigel", quantity: 1}], customer: [{name: "Linus Pellesson"},{phone: "074-21 23 124"}]},
-  {id: 11, time: "20:39:16", ordernr: 4522, order: [{type: "Snigel deluxe", quantity: 1}, {type: "Currysnigel", quantity: 3}], customer: [{name: "Linus Pellesson"},{phone: "074-21 23 124"}]}, 
-  {id: 12, time: "20:39:16", ordernr: 4521, order: [{type: "Vitlökssnigel", quantity: 2}, {type: "Snigel almond", quantity: 2}], customer: [{name: "Linus Pellesson"},{phone: "074-21 23 124"}]}, 
-  {id: 13, time: "20:39:16", ordernr: 4520, order: [{type: "Vitlökssnigel", quantity: 2}, {type: "Currysnigel", quantity: 2}], customer: [{name: "Linus Pellesson"},{phone: "074-21 23 124"}]}, 
-  {id: 14, time: "20:39:16", ordernr: 4519, order: [{type: "Vitlökssnigel", quantity: 2}, {type: "Currysnigel", quantity: 2}], customer: [{name: "Linus Pellesson"},{phone: "074-21 23 124"}]}, 
-  {id: 15, time: "20:39:16", ordernr: 4518, order: [{type: "Vitlökssnigel", quantity: 2}, {type: "Currysnigel", quantity: 2}], customer: [{name: "Linus Pellesson"},{phone: "074-21 23 124"}]}, 
-  {id: 16, time: "20:39:16", ordernr: 4517, order: [{type: "Vitlökssnigel", quantity: 2}, {type: "Currysnigel", quantity: 2}], customer: [{name: "Linus Pellesson"},{phone: "074-21 23 124"}]
-}]
+  { id: 9, time: "20:39:16", ordernr: 4524, comment: "Hälften av brödet ska vara blött tack", order: [{ type: "Vitlökssnigel", quantity: 2 }, { type: "Currysnigel", quantity: 4 }], customer: { name: "Linus Pellesson", phone: "074-21 23 124" } },
+
+  { id: 10, time: "20:39:16", ordernr: 4523, comment: "Hälften av brödet ska vara blött tack", order: [{ type: "Vitlökssnigel", quantity: 2 }, { type: "Grässnigel", quantity: 1 }], customer: { name: "Linus Pellesson", phone: "074-21 23 124" } },
+
+  { id: 11, time: "20:39:16", ordernr: 4522, order: [{ type: "Snigel deluxe", quantity: 1 }, { type: "Currysnigel", quantity: 3 }], customer: { name: "Linus Pellesson", phone: "074-21 23 124" } },
+
+  { id: 12, time: "20:39:16", ordernr: 4521, order: [{ type: "Vitlökssnigel", quantity: 2 }, { type: "Snigel almond", quantity: 2 }], customer: { name: "Linus Pellesson", phone: "074-21 23 124" } },
+
+  { id: 13, time: "20:39:16", ordernr: 4520, order: [{ type: "Vitlökssnigel", quantity: 2 }, { type: "Currysnigel", quantity: 2 }], customer: { name: "Linus Pellesson", phone: "074-21 23 124" } },
+
+  { id: 14, time: "20:39:16", ordernr: 4519, order: [{ type: "Vitlökssnigel", quantity: 2 }, { type: "Currysnigel", quantity: 2 }], customer: { name: "Linus Pellesson", phone: "074-21 23 124" } },
+  {
+    id: 15,
+    time: "20:39:16",
+    ordernr: 4518,
+    order: [{ type: "Vitlökssnigel", quantity: 2 }, { type: "Currysnigel", quantity: 2 }], customer: { name: "Linus Pellesson", phone: "074-21 23 124" }
+  },
+
+  {
+    id: 16, time: "20:39:16", ordernr: 4517, order: [{ type: "Vitlökssnigel", quantity: 2 }, { type: "Currysnigel", quantity: 2 }], customer: { name: "Linus Pellesson" , phone: "074-21 23 124" }
+  }]
 
 const AdminArchive = () => {
 
   return (
     <div className="admin-wrapper">
       <section className="archive-list">
-        {archivedOrders.map((order, i) =>{
+        {archivedOrders.map((order, i) => {
           return (
-            <OrderItem key={i} order={order} index={i}/>
+            <OrderItem key={i} order={order} index={i} />
           )
         })}
       </section>
-    </div> 
+    </div>
+
   )
 }
 export default AdminArchive;
--- src/components/OrderItem/OrderItem.tsx
+++ src/components/OrderItem/OrderItem.tsx
@@ -5,56 +5,59 @@ import { useState } from "react";
 
 
 type Props = {
-    order: any;
-    index: number;
+  order: any;
+  index: number;
 }
 
-const OrderItem = ({order, index}: Props) => {
-    const [className, setClassName] = useState<string>('accordion')
-    const [accordionOpen, setAccordionOpen] = useState<boolean>(false);
+const OrderItem = ({ order, index }: Props) => {
+  const [className, setClassName] = useState<string>('accordion')
+  const [accordionOpen, setAccordionOpen] = useState<boolean>(false);
 
-    const toggleDrop = () => {
-      if (!accordionOpen) {
-          setClassName("accordion-open");
-          setAccordionOpen(true);
-        } else {
-          setClassName("accordion");
-          setAccordionOpen(false);
-        }        
+  const toggleDrop = () => {
+    if (!accordionOpen) {
+      setClassName("accordion-open");
+      setAccordionOpen(true);
+    } else {
+      setClassName("accordion");
+      setAccordionOpen(false);
     }
+  }
+  return (
+    <div onClick={toggleDrop} key={index} className="archive-list-item">
+      <section className="list-item-details">
+        <p>kl {order.time}</p>
+        <h2>{order.ordernr}</h2>
+        {accordionOpen ?
+          <figure className="archive-list-drop-up">
+            <IoMdArrowDropdown />
+          </figure> :
+          <figure className="archive-list-drop">
+            <IoMdArrowDropdown />
+          </figure>
+        }
 
-    return (
-        <div onClick={toggleDrop} key={index}className="archive-list-item">
-              <section className="list-item-details">
-                <p>kl {order.time}</p>
-                <h2>{order.ordernr}</h2>
-                <figure  className="archive-list-drop">
-                  <IoMdArrowDropdown />
-                </figure>
-              </section>
-              <section className={className}>
-                <div>
-                  <h2>Order</h2>
-                  {order.order.map((order:any, i:number)=> {
-                    return (
-                    <p key={i}>{order.type}</p>
-                    )
-                  })}
-                </div>
-                <div>
-                  <h2>Beställare</h2>
-                  {order.customer.map((customer:any, i:number)=> {
-                    return (
-                    <div key={i}>
-                      <p>{customer.name}</p>
-                      <p>{customer.phone}</p>
-                    </div>
-                    )
-                  })}
-                </div>
-              </section>
-            </div>
-    )
+      </section>
+      <section className={className}>
+        <div>
+          <h2>Order</h2>
+          {order.order.map((order: any, i: number) => {
+            return (
+              <p className="order-items" key={i}> <span> {order.quantity} st </span>{order.type}</p>
+            )
+          })}
+        </div>
+        <div className="customer">
+          <h2>Beställare</h2>
+          <p>{order.customer.name}</p>
+          <p>{order.customer.phone}</p>
+        </div>
+        <div className="order-comment">
+          <h2>Kommentar</h2>
+          <p>{order.comment}</p>
+        </div>
+      </section>
+    </div>
+  )
 }
 
 export default OrderItem
