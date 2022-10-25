dfafb3d119c0746fbc32bfc8197451cdf382da51
Linus Eklind <76260367+Eklinden@users.noreply.github.com>
2022-10-25 10:50:03 +0200

    updates admin tsx

--- backend/server.ts
+++ backend/server.ts
@@ -1,24 +1,26 @@
-import express, { Request, Response } from "express";
+import cors from 'cors';
+import express, { Request, Response } from 'express';
 const app = express();
 const PORT = 1337;
-import cors from "cors"
 // const cors = require("cors")
-import menuRoute from "./routes/menu.js";
-import ordersRoute from "./routes/orders.js";
-import { join, dirname } from "path";
-import { fileURLToPath } from "url";
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import menuRoute from './routes/menu.js';
+import ordersRoute from './routes/orders.js';
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const staticPath = join(__dirname, "../../dist");
-
+const staticPath = join(__dirname, '../../dist');
 
 //Middleware
 app.use(express.json());
-
-app.use(cors())
+app.use((req, res, next) => {
+	console.log(req.httpVersion);
+	next();
+});
+app.use(cors());
 
 app.use((req, res, next) => {
-  console.log(`${req.method}  ${req.url} `, req.body);
-  next();
+	console.log(`${req.method}  ${req.url} `, req.body);
+	next();
 });
 
 // Obs! express.static bör ligga först, när man får många statiska filer
@@ -26,12 +28,12 @@ app.use(express.static(staticPath));
 
 // Routes / endpoints
 
-app.use("/api/menu", menuRoute);
-app.use("/api/orders", ordersRoute);
+app.use('/api/menu', menuRoute);
+app.use('/api/orders', ordersRoute);
 // app.use('/api/archive', archiveRoute)
 // app.use('/api/credentials', credentialsRoute)
 
 // Starta servern
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}.`);
+	console.log(`Server is listening on port ${PORT}.`);
 });
--- src/components/AdminOrders/AdminOrders.tsx
+++ src/components/AdminOrders/AdminOrders.tsx
@@ -1,130 +1,183 @@
-import "./AdminOrders.scss";
-import { useState, useEffect } from "react";
-import { IoIosAdd, IoIosRemove } from "react-icons/io";
-import { Order } from "../../models/data";
+import cors from 'cors';
+import { useEffect, useState } from 'react';
+import { IoIosAdd, IoIosRemove } from 'react-icons/io';
 import Data from '../../data/data.json';
-
-
+import { Order } from '../../models/data';
+import './AdminOrders.scss';
 
 const AdminOrders = () => {
-  const [allOrders, setAllOrders] = useState<Order[]>([{
-    orderNumber: 231354,
-    orderItems: [
-      {
-        menuItem: {
-          id: '200',
-          name: 'Tradition',
-          price: 149,
-          description: 'Autentiskt franskt lantrecept på escargots med persilja, vitlök och smör',
-          image: 'https://www.sbs.com.au/food/sites/sbs.com.au.food/files/img_7379-snails.jpg'
-        },
-        amount: 2
-      }
-    ],
-    customerComment: "HEY ",
-    customer: "Jesus",
-    phoneNumber: 57357357
-  }]);
+	const [allOrders, setAllOrders] = useState<Order[]>([
+		{
+			orderNumber: 231354,
+			orderItems: [
+				{
+					menuItem: {
+						id: '200',
+						name: 'Tradition',
+						price: 149,
+						description:
+							'Autentiskt franskt lantrecept på escargots med persilja, vitlök och smör',
+						image: 'https://www.sbs.com.au/food/sites/sbs.com.au.food/files/img_7379-snails.jpg',
+					},
+					amount: 2,
+				},
+			],
+			customerComment: 'HEY ',
+			customer: 'Jesus',
+			phoneNumber: 57357357,
+		},
+	]);
 
-  const [isSelected, setIsSelected] = useState<any>({ id: allOrders[0].orderNumber });
+	const [isSelected, setIsSelected] = useState<any>({
+		id: allOrders[0].orderNumber,
+	});
 
-  const selectedOrderArr: any = allOrders[0].orderItems;
+	const selectedOrder: Order = {
+		orderNumber: 1,
+		orderItems: [],
+		customerComment: '',
+		customer: '',
+		phoneNumber: 0,
+	};
 
-  const allEscargotsInMenu = Data.array.menu[0].menuItems
-  const allKidsInMenu = Data.array.menu[1].menuItems
-  const allDrinksInMenu = Data.array.menu[2].menuItems
-  const allDessertsInMenu = Data.array.menu[3].menuItems
+	const allEscargotsInMenu = Data.array.menu[0].menuItems;
+	const allKidsInMenu = Data.array.menu[1].menuItems;
+	const allDrinksInMenu = Data.array.menu[2].menuItems;
+	const allDessertsInMenu = Data.array.menu[3].menuItems;
 
-  const fetchOrders = async () => {
-    const response = await fetch('/api/orders');
-    console.log("response", response) //ERROR, why?
-    const data: Order[] = await response.json();
-    // console.log("data", data)
-    setAllOrders(data)
-  }
+	const fetchOrders = async () => {
+		const response = await fetch('/api/orders', {
+			mode: 'cors',
+		});
+		console.log('response', response); //ERROR, why?
+		const data: Order[] = await response.json();
+		console.log('data', data);
+		// console.log("data", data)
+		setAllOrders(data);
+	};
 
-  useEffect(() => {
-    // fetch('http://localhost:1337/api/orders', {mode: 'cors'})
-    // .then(response => response.json())
-    // .then(setAllOrders)
-    // .catch(()=>console.log("error"))
-    fetchOrders()
-  }, [])
+	useEffect(() => {
+		// fetch('http://localhost:1337/api/orders')
+		// 	.then(response => response.json())
+		// 	.then(setAllOrders)
+		// 	.catch(() => console.log('error'));
+		fetchOrders();
+	}, []);
 
+	return (
+		<div className='admin-wrapper'>
+			<section className='orders-list'>
+				<ul>
+					{allOrders.map((order: Order, i: number) => {
+						return (
+							<li
+								key={i}
+								className={
+									order.orderNumber === isSelected
+										? 'selected'
+										: ''
+								}
+								onClick={() => {
+									setIsSelected(order.orderNumber);
+								}}>
+								{order.orderNumber}
+							</li>
+						);
+					})}
+				</ul>
+			</section>
+			<div className='line'></div>
+			<section className='order-details'>
+				{selectedOrder.orderItems.map((order, i: number) => {
+					return (
+						<article key={i} className='order-detail-item'>
+							<p>{order.menuItem.name}</p>
+							<div className='quantity-container'>
+								<button className='decrease'>
+									{' '}
+									<IoIosRemove />
+								</button>
+								<p className='quantity'>{order.amount}</p>
+								<button className='decrease'>
+									{' '}
+									<IoIosAdd />
+								</button>
+							</div>
+						</article>
+					);
+				})}
+			</section>
+			<h2 className='subheading'>Kundinformation</h2>
+			<section className='customer-info'>
+				<p>
+					Meddelande från beställare:
+					<span className='message'>
+						{selectedOrder.customerComment}
+					</span>
+				</p>
+				<p>
+					Kund: <span>{selectedOrder.customer}</span>
+				</p>
+				<p>
+					Telefon: <span>{selectedOrder.phoneNumber}</span>
+				</p>
+			</section>
+			<h2 className='subheading'>Lägg till maträtt</h2>
+			<section className='add-to-order-container'>
+				{allEscargotsInMenu.map((order: any, i: number) => {
+					return (
+						<div key={i}>
+							<p>{order.name}</p>
+							<button>
+								{' '}
+								<IoIosAdd />
+							</button>
+						</div>
+					);
+				})}
+				{allKidsInMenu.map((order: any, i: number) => {
+					return (
+						<div key={i}>
+							<p>{order.name}</p>
+							<button>
+								<IoIosAdd />{' '}
+							</button>
+						</div>
+					);
+				})}
+				{allDrinksInMenu.map((order: any, i: number) => {
+					return (
+						<div key={i}>
+							<p>{order.name}</p>
+							<button>
+								{' '}
+								<IoIosAdd />
+							</button>
+						</div>
+					);
+				})}
+				{allDessertsInMenu.map((order: any, i: number) => {
+					return (
+						<div key={i}>
+							<p>{order.name}</p>
+							<button>
+								{' '}
+								<IoIosAdd />
+							</button>
+						</div>
+					);
+				})}
+			</section>
+			<h2 className='subheading'>Meddelande till kocken</h2>
+			<section className='message-to-chef'>
+				<form action=''>
+					<textarea name='' id='' cols={40} rows={5}></textarea>
+				</form>
+			</section>
 
-  return (
-    <div className="admin-wrapper">
-      <section className="orders-list">
-        <ul>
-          {allOrders.map((order: any, i: number) => {
-            return (
-              <li key={i} className={order.id === isSelected.id ? "selected" : ""} onClick={() => { setIsSelected({ id: order.id }) }}>{order.ordernr}</li>
-            )
-          })}
-        </ul>
-      </section>
-      <div className="line"></div>
-      <section className="order-details">
-        {selectedOrderArr.map((order: any, i: number) => {
-          return (
-            <article key={i} className="order-detail-item">
-              <p>{order.type}</p>
-              <div className="quantity-container">
-                <button className="decrease"> < IoIosRemove /></button>
-                <p className="quantity">{order.quantity}</p>
-                <button className="decrease"> < IoIosAdd /></button>
-              </div>
-            </article>)
-        })}
-      </section>
-      <h2 className='subheading'>Kundinformation</h2>
-      <section className="customer-info">
-        <p>Meddelande från beställare:
-          <span className="message">"Ingen senap tack, hälften av brödet ska vara blött"</span>
-        </p>
-        <p>Kund: <span>Linus Pellesson</span></p>
-        <p>Telefon: <span>070-177 14 32</span></p>
-      </section>
-      <h2 className='subheading'>Lägg till maträtt</h2>
-      <section className="add-to-order-container">
-        {allEscargotsInMenu.map((order: any, i: number) => {
-          return (
-            <div key={i}>
-              <p>{order.name}</p>
-              <button> < IoIosAdd /></button>
-            </div>)
-        })}
-        {allKidsInMenu.map((order: any, i: number) => {
-          return (
-            <div key={i}>
-              <p>{order.name}</p>
-              <button>< IoIosAdd /> </button>
-            </div>)
-        })}
-        {allDrinksInMenu.map((order: any, i: number) => {
-          return (
-            <div key={i}>
-              <p>{order.name}</p>
-              <button> < IoIosAdd /></button>
-            </div>)
-        })}
-        {allDessertsInMenu.map((order: any, i: number) => {
-          return (
-            <div key={i}>
-              <p>{order.name}</p>
-              <button > < IoIosAdd /></button>
-            </div>)
-        })}
-      </section>
-      <h2 className='subheading'>Meddelande till kocken</h2>
-      <section className='message-to-chef'>
-        <form action="">
-          <textarea name="" id="" cols={40} rows={5}></textarea>
-        </form>
-      </section>
+			<button className='confirm-btn'>Bekräfta</button>
+		</div>
+	);
+};
 
-      <button className="confirm-btn">Bekräfta</button>
-    </div>
-  )
-}
 export default AdminOrders;
