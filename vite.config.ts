5904f0c9d08c16aeeafd32cc1f4836ed122e3597
Williamlof <83575469+Williamlof@users.noreply.github.com>
2022-10-27 14:45:51 +0200

    docker

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
+<<<<<<< Updated upstream
       "/api": "http://localhost:5174/"
     }
   }
 })
+=======
+      "/api": "http://localhost:6969/",
+    },
+  },
+});
+>>>>>>> Stashed changes
