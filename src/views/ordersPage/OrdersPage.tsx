7ffe9a302651bb4653deae412ca889f09505850d
Williamlof <83575469+Williamlof@users.noreply.github.com>
2022-10-31 10:46:47 +0100

    Scss ändringar
    Headern byter nu inte storlek när main är för liten. Satt min-height på de som inte tar upp tillräckligt med yta.

--- src/views/ordersPage/OrdersPage.tsx
+++ src/views/ordersPage/OrdersPage.tsx
@@ -2,7 +2,7 @@ import "../ordersPage/OrdersPage.scss";
 
 const OrderPage = () => {
   return (
-    <main>
+    <main className="main">
       <h1>Här kommer användaren att se sin beställning</h1>
     </main>
   );
