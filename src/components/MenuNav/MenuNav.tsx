3a359a3c7fff7571fc249539ef9c3a3c31d9de8a
Linus Eklind <76260367+Eklinden@users.noreply.github.com>
2022-11-07 09:42:23 +0100

    make scroll work on homepage at desktop

--- src/components/MenuNav/MenuNav.tsx
+++ src/components/MenuNav/MenuNav.tsx
@@ -8,6 +8,7 @@ const MenuNav = () => {
   const [dessertSelected, setdessertSelected] = useState("");
 
   window.addEventListener('scroll', () => {
+    console.log(window.pageYOffset)
     if (window.innerWidth < 730) {
       if (window.pageYOffset > 240 && window.pageYOffset < 1470) {
         setbeveragesSelected("");
@@ -31,22 +32,22 @@ const MenuNav = () => {
         setfoodSelected("");
       }
     } else if (window.innerWidth > 730) {
-      if (window.pageYOffset > 320 && window.pageYOffset < 1441) {
+      if (window.pageYOffset > 300 && window.pageYOffset < 1400) {
         setbeveragesSelected("");
         setkidsSelected("");
         setdessertSelected("");
         setfoodSelected("active");
-      } else if (window.pageYOffset > 1442 && window.pageYOffset < 2030) {
+      } else if (window.pageYOffset > 1401 && window.pageYOffset < 1989) {
         setbeveragesSelected("");
         setkidsSelected("active");
         setdessertSelected("");
         setfoodSelected("");
-      } else if (window.pageYOffset > 2031 && window.pageYOffset < 2900) {
+      } else if (window.pageYOffset > 1990 && window.pageYOffset < 2819) {
         setbeveragesSelected("active");
         setkidsSelected("");
         setdessertSelected("");
         setfoodSelected("");
-      } else if (window.pageYOffset > 2901) {
+      } else if (window.pageYOffset > 2820) {
         setbeveragesSelected("");
         setkidsSelected("");
         setdessertSelected("active");
@@ -59,28 +60,28 @@ const MenuNav = () => {
     if (window.innerWidth < 730) {
       window.scrollTo(0, 240)
     } else if (window.innerWidth > 730) {
-      window.scrollTo(0, 323)
+      window.scrollTo(0, 315)
     }
   }
   function scrollToKid() {
     if (window.innerWidth < 730) {
       window.scrollTo(0, 1472)
     } else if (window.innerWidth > 730) {
-      window.scrollTo(0, 1444)
+      window.scrollTo(0, 1405)
     }
   }
   function scrollToBeverage() {
     if (window.innerWidth < 730) {
       window.scrollTo(0, 2122)
     } else if (window.innerWidth > 730) {
-      window.scrollTo(0, 2033)
+      window.scrollTo(0, 2005)
     }
   }
   function scrollToDessert() {
     if (window.innerWidth < 730) {
       window.scrollTo(0, 2922)
     } else if (window.innerWidth > 730) {
-      window.scrollTo(0, 2904)
+      window.scrollTo(0, 2835)
     }
   }
 
